87f8926b92632fc830c0bf97d516fb9cb399aa3b
elecshen <51473599+elecshen@users.noreply.github.com>
2023-09-06 19:38:48 +0700

    Добавлен класс Entry для хранения записей таблицы

--- LB1/Program.cs
+++ LB1/Program.cs
@@ -1,5 +1,23 @@
-﻿namespace LB1
+﻿namespace AoIS_LB1
 {
+	public class Entry
+	{
+		public int Key { get; private set; }
+		private object[] data;
+		public object[] Data { get { return data; } }
+
+		Entry(int rowNum, object[] data)
+		{
+			Key = rowNum;
+			this.data = data;
+		}
+
+		void SetCell(int colNum, object cell)
+		{
+			data[colNum] = cell;
+		}
+	}
+
 	internal class Program
 	{
 		static void Main(string[] args)
