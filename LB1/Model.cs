4f9c0468fd3510cf08938a25311fd2255831f4c2
elecshen <51473599+elecshen@users.noreply.github.com>
2023-09-09 17:41:24 +0700

    Запись обощается как объект CSVObject Класс объекта и интерфейс можели вынесены в отдельный файл

---
+++ LB1/Model.cs
@@ -0,0 +1,31 @@
+﻿namespace AoIS_LB1
+{
+	public class CSVObject
+	{
+		public string Name { get; set; }
+		public string Role { get; set; }
+		public bool IsHaveBag { get; set; }
+		public double Length { get; set; }
+		public double Width { get; set; }
+		public int Number { get; set; }
+
+		public CSVObject(string name, string role, bool isHaveBag, double length, double width, int number)
+		{
+			Name = name;
+			Role = role;
+			IsHaveBag = isHaveBag;
+			Length = length;
+			Width = width;
+			Number = number;
+		}
+	}
+
+	interface IAois_model
+	{
+		List<object> GetTable();
+		object FindEntry(int key);
+		bool EditEntry(object entry);
+		bool RemoveEntry(object entry);
+		bool AddEntry(object entry);
+	}
+}
--- LB1/Program.cs
+++ LB1/Program.cs
@@ -1,17 +1,5 @@
 ﻿namespace AoIS_LB1
 {
-	public class CSVEntry
-	{
-		public int Key { get; private set; }
-		public object Data { get; private set; }
-
-		CSVEntry(int rowNum, object data)
-		{
-			Key = rowNum;
-			Data = data;
-		}
-	}
-
 	internal class Program
 	{
 		static void Main(string[] args)
