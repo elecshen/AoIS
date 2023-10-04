44831cf4867321f741be4357f9674c71dda0d3a3
elecshen <51473599+elecshen@users.noreply.github.com>
2023-10-04 19:12:16 +0700

    Добавлен WPF View

---
+++ ClientWPFApp/Views/TablePage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Controls;
+
+namespace ClientWPFApp.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для TablePage.xaml
+    /// </summary>
+    public partial class TablePage : Page
+    {
+        public DataGrid TableG { get; set; }
+        public DataGrid TableE { get; set; }
+        public TablePage()
+        {
+            InitializeComponent();
+			TableG = TableGrid;
+            TableE = EditGrid;
+		}
+		void DataGrid_LoadingRow(object sender, DataGridRowEventArgs e)
+		{
+			e.Row.Header = (e.Row.GetIndex()+1).ToString();
+		}
+	}
+}
